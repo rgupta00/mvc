96ac59bb331222bee44ff1ebe4917ef619b83f77
rgupta00 <rgupta.mtech@gmail.com>
2020-12-28 16:12:14 +0530

    Update HelloController.java
    code is improved

--- src/main/java/com/demo/HelloController.java
+++ src/main/java/com/demo/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public ModelAndView hello(ModelAndView mv) {
-		mv.addObject("msg", "hello to spring mvc is chaged");
+		mv.addObject("msg", "hello to spring mvc is chaged done corectly this time");
 		System.out.println("sysout is changed by dev1 and dev2, working fine changed again");
 		mv.setViewName("hello");
 		return mv;
